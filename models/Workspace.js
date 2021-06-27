a08563af409913c6cf7a8bd44109a24e5ad800a6
Atara Frieman <fr5836316@gmail.com>
2021-06-27 22:53:46 +0300

    add user models

--- models/Workspace.js
+++ models/Workspace.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
  ,Base = require('./Base');
 
 const WorkspaceSchema = Base.discriminator('Workspace', mongoose.Schema({
-    
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project'
+    }]
 }))
 
 module.exports = mongoose.model('Workspace')
